8c150d948d826e72ccde5eee2635ed0cec1e5054
Jeremy Devore <jeremy@JeremyDevore-2025FZ.local>
2026-02-27 22:19:12 -0600

    config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Tapping term - how long before a hold is registered (ms)
+#define MIRYOKU_TAPPING_TERM 280
+
+// Use Mac-style clipboard shortcuts (Cmd+C/V/X/Z)
+#define MIRYOKU_CLIPBOARD_MAC
